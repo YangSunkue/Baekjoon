cf6e7d3988c69e41fc5d33e4537fd04fd057210a
Yangsunkue <ysk9526@gmail.com>
2024-03-23 19:39:00 +0900

    수 정렬하기2

---
+++ baekjoon_2751.py
@@ -0,0 +1,13 @@
+import sys
+
+n = int(sys.stdin.readline())
+
+numList = []
+
+for i in range(n):
+    numList.append(int(sys.stdin.readline()))
+
+numList.sort()
+
+for i in numList:
+    print(i)
