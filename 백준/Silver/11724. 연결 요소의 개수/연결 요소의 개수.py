5198d47de78f82d17e884820303e0bb10b7d78d7
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-03-30 17:07:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 704 ms, Memory: 65076 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,27 +1,28 @@
 import sys
-sys.setrecursionlimit(10 ** 9)
+sys.setrecursionlimit(10 ** 9)  # 재귀함수 최대깊이 늘려놓기
 
-N, M = map(int, sys.stdin.readline().split())
-graph = [[] for _ in range(N + 1)]  # 노드번호와 인덱스번호를 맞추기 위해 + 1
-for _ in range(M):
+N, M = map(int, sys.stdin.readline().split())  # 노드개수, 간선개수
+
+graph = [[] for _ in range(N + 1)]  # 노드번호와 인덱스를 맞추기 위해 + 1
+for _ in range(M):  # 연결정보 입력받기
     root, edge = map(int, sys.stdin.readline().split())
     graph[root].append(edge)
     graph[edge].append(root)
 
-visited = [False] * (N + 1)  # 노드번호와 인덱스번호를 맞추기 위해 + 1
-visited[0] = True  # 모든 노드에 들렸는지 편하게 확인하기 위해 안쓰는 0번을 True로 초기화해준다
-conn = 0
+visited = [False] * (N + 1)  # 노드번호와 인덱스를 맞추기 위해 + 1
+visited[0] = True  # 마지막에 False 존재 여부를 검사하기 위해 미리 안쓰는 0번 인덱스도 True로 초기화 해놓음
+conn = 0  # 연결 요소 갯수 세는 변수
 
-def dfs(graph, v, visited):
-    visited[v] = True  # 들렸으니 방문처리
+def dfs(graph, v, visited):  # dfs탐색 시작
+    visited[v] = True
 
-    for i in graph[v]:  # 인접 노드 순회하기
-        if visited[i] == False:  # 인접노드 안들렸을경우
+    for i in graph[v]:
+        if visited[i] == False:
             dfs(graph, i, visited)
 
-while False in visited:  # False가 하나라도 있다면 반복문 진행(연결 요소가 2개 이상 있다고 판단)
-    start = visited.index(False)  # 들리지 않은 노드를 시작 노드로 설정
-    dfs(graph, start, visited)  # dfs 돌리기
-    conn += 1 # 한번 돌릴때마다 연결 요소 1개씩 증가
+while False in visited:  # 만약 탐색했는데 안간곳이 있었다 -> 연결 요소가 추가로 있다 -> 안간 노드를 시작으로 dfs 한번 더
+    start = visited.index(False)
+    dfs(graph, start, visited)
+    conn += 1  # 한번 탐색할 때마다 연결요소 + 1
 
-print(conn)  # 계산된 연결 요소 개수 출력
+print(conn)  # 계산된 연결요소 개수 출력
