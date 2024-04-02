552082b77618503866b47eb39826735bd94211df
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-04-02 20:34:16 +0900

    [Gold IV] Title: 미로만들기, Time: 48 ms, Memory: 33188 KB -BaekjoonHub

--- 백준/Gold/2665. 미로만들기/미로만들기.py
+++ 백준/Gold/2665. 미로만들기/미로만들기.py
@@ -1,49 +1,47 @@
 import heapq
 import sys
 
-input = sys.stdin.readline
 INF = int(1e9)
-N = int(input())  # N * N의 정사각형
+input = sys.stdin.readline
+N = int(input())
 board = []
-for _ in range(N):
+for _ in range(N):  # 미로 입력받기
     board.append(list(map(int, input().strip())))
-distroy = [[INF for i in range(N)] for i in range(N)]  # 시작점부터 distroy좌표까지 오면서 부순 값을 저장한다.
-visited = [[False for i in range(N)] for i in range(N)]
+distroy = [[INF for _ in range(N)] for _ in range(N)]  # 부순갯수
+visited = [[False for _ in range(N)] for _ in range(N)]  # 방문목록
 
-dx = [-1, 1, 0, 0]
-dy = [0, 0, -1, 1]
-
-def goingNogoing(x, y):
+def goingNogoing(x, y):  # 갈지말지 정하는함수
     if 0 <= x < N and 0 <= y < N and visited[x][y] == False:
         return True
     return False
 
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
 def dijkstra():
     queue = []
     heapq.heappush(queue, (0, 0, 0))  # 부순갯수, x, y
+    visited[0][0] = True
+    distroy[0][0] = 0
 
     while queue:
         for _ in range(len(queue)):
-            dist, hx, hy = heapq.heappop(queue)  # 부순갯수, x, y
-            if hx == (N - 1) and hy == (N - 1):  # 도착했을 경우 ( 최소힙써서 도착했으면 무조건 최소로부순값임 )
-                return dist  # 부순 갯수 리턴
+            dist, cx, cy = heapq.heappop(queue)  # 큐에서 현재까지 부순갯수, x, y 빼기
+            if cx == N - 1 and cy == N - 1:  # 도착했다면 부순갯수 리턴
+                return dist
+            
+            for i in range(4):  # 상하좌우 반복
+                nx = cx + dx[i]
+                ny = cy + dy[i]
 
-            else:
-                for i in range(4):  # 상하좌우 반복
-                    nx = hx + dx[i]
-                    ny = hy + dy[i]
+                if goingNogoing(nx, ny):
+                    if board[nx][ny] == 0:  # 다음방이 검은방이면
+                        cost = dist + 1  
+                        distroy[nx][ny] = cost  # 부순갯수 +1 해서 테이블 갱신
+                        heapq.heappush(queue, (cost, nx, ny))  # 갱신했으니 큐에 넣기
+                        visited[nx][ny] = True  # 방문체크
+                    else:
+                        distroy[nx][ny] = dist  # 부순갯수 그대로 테이블 갱신
+                        heapq.heappush(queue, (dist, nx, ny))  # 갱신했으니 큐에 넣기
+                        visited[nx][ny] = True  # 방문체크
 
-                    if goingNogoing(nx, ny):
-                        if board[nx][ny] == 0:  # 검은방일 때
-                            cost = dist + 1  # 현재노드까지 값에 + 1한 값을 계산
-                            # if cost < distroy[nx][ny]:  # 그 값이 다음진행할 좌표값보다 작다면 업뎃하고 큐에 추가
-                            distroy[nx][ny] = cost
-                            heapq.heappush(queue, (cost, nx, ny))
-                            visited[nx][ny] = True
-                            
-                        else:  # 하얀방일 때
-                            distroy[nx][ny] = dist  # 다음 경로에 현재 부순값 넣기
-                            heapq.heappush(queue, (dist, nx, ny))
-                            visited[nx][ny] = True
-    
 print(dijkstra())
