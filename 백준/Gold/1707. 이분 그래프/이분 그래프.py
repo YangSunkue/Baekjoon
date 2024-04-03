ccc775400678f33c6311082a492607848de64fd6
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-04-03 16:36:53 +0900

    [Gold IV] Title: 이분 그래프, Time: 1228 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.py
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.py
@@ -1,34 +1,33 @@
 import sys
-sys.setrecursionlimit(10**9)
+sys.setrecursionlimit(10 ** 9)
 input = sys.stdin.readline
 
-def DFS(graph, start, visited, group): # group값으로 그룹을 구분한다
+# 이분 그래프 판별 DFS 함수
+def DFS(graph, start, visited, group): 
     visited[start] = group
 
     for i in graph[start]:
-        if visited[i] == 0:  # 아직 방문 안 했을 경우
-            result = DFS(graph, i, visited, -group)  # 그룹을 바꿔서 재귀호출
-            if result == False:  # 하위호출에서 False가 나왔다면 이미 이분 그래프 아니므로 함수 종료시키기
-                return False
-        else: 
-            if visited[i] == group:  # 만약 현재그룹과 같다면 False 리턴( 이분 그래프 아님 )
-                return False
-    return True  # 이분 그래프라면 True 리턴
+        if visited[i] == 0:  # 방문 안 한 곳이라면
+            result = DFS(graph, i, visited, -group)
+            if not result: return False  # 하위에서 이분 그래프 아니라고 판단했다면 함수 종료
+        elif visited[i] == group:  # 인접노드인데 그룹이 같을경우 이분그래프 아님
+            return False
+    return True
 
+# 입력받은 갯수만큼 그래프를 검사한다
 for _ in range(int(input())):
     # 사전준비
     N, E = map(int, input().split())  # 노드, 간선
-    graph = [[] for _ in range(N + 1)]
+    graph = [[] for _ in range(N + 1)]  # 연결정보
     for _ in range(E):
         a, b = map(int, input().split())
         graph[a].append(b)
         graph[b].append(a)
-    visited = [0 for _ in range(N + 1)]
+    visited = [0 for _ in range(N + 1)]  # 방문체크 리스트
 
-    # DFS 진행, 그래프가 끊어져 있을 수도 있으므로 가지 않은 노드가 있다면 해당 노드부터 DFS 진행
+    # 모든 노드에 대하여 DFS 진행
     for i in range(1, N + 1):
-        if visited[i] == 0:
+        if visited[i] == 0:  # 확인 안 한 노드가 있다면 진행
             result = DFS(graph, i, visited, 1)
-            if result == False:
-                break
-    print("YES") if result else print("NO")
+            if not result: break
+    print("YES") if result else print("NO")  # 이분 그래프면 YES, 아니면 NO 출력
