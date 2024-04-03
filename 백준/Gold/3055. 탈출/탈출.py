f08227265823892e9bc78d29412f03f7246b2cb3
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-04-03 21:10:31 +0900

    [Gold IV] Title: 탈출, Time: 64 ms, Memory: 34112 KB -BaekjoonHub

--- 백준/Gold/3055. 탈출/탈출.py
+++ 백준/Gold/3055. 탈출/탈출.py
@@ -5,13 +5,12 @@ input = sys.stdin.readline
 
 R, C = map(int, input().split())  # 행, 열
 forest = [list(input().strip()) for _ in range(R)]
+visited = [[-1] * C for _ in range(R)]  # -1로 설정해두고 도치의 이동 과정을 입력할것이다
 
-visited = [[-1] * C for _ in range(R)]
+
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
-
 queue = deque()
-
 for x in range(R):
     for y in range(C):
         if forest[x][y] == '*':  # 물 좌표를 큐의 앞에 추가 ( 물이 먼저 가야 한다 )
@@ -41,11 +40,11 @@ def dochiKill():
             if goingNogoing(nx, ny, visited, forest, who):  # 갈수 있다면
                 if forest[nx][ny] == 'D':  # 비버집 도착했으면 결과 출력
                     return visited[x][y] + 1
-                else:
-                    visited[nx][ny] = visited[x][y] + 1
+                
+                visited[nx][ny] = visited[x][y] + 1  # 다음 진출경로에 시간 입력
                 forest[nx][ny] = who  # 다음 칸을 도치/물로 변경 (진출한다)
                 queue.append((nx, ny))
-    return "KAKTUS"
 
+    return "KAKTUS"  # 숲을 다 돌았는데 비버집에 못 갔다면 KAKTUS 리턴
 
 print(dochiKill())
