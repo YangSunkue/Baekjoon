afa6c5885378120c490670c7143d48ebc4850ef9
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-04-03 13:49:20 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 408 ms, Memory: 63668 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -2,38 +2,30 @@ import heapq
 import sys
 
 input = sys.stdin.readline
-
 N, E = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
 for _ in range(E):
-    a, b, c = map(int, input().split())  # 출발, 도착, 비용
+    a, b, c = map(int, input().split())
     graph[a].append((c, b))
     graph[b].append((c, a))
-
 visited = [False] * (N + 1)
-distance = 0
-def prim(graph, start):  # 그래프, 시작노드 입력받기
-    global distance
 
-    mst = []
+def prim(graph, start):
     visited[start] = True
+    distance = 0
+    edge = [(cost, start, end) for cost, end in graph[start]]  # 시작노드의 인접노드에 대한 정보를 미리 리스트에 넣는다
+    heapq.heapify(edge)  # 리스트를 우선순위 큐(최소힙)로 변경한다.
 
-    edges = [(cost, start, end) for cost, end in graph[start]]
-    heapq.heapify(edges)
-
-    while edges:
-        cost, start, end = heapq.heappop(edges)
-
+    while edge:
+        cost, start, end = heapq.heappop(edge)
         if visited[end] == False:
             visited[end] = True
-            mst.append((start, end, cost))
             distance += cost
 
-            for nextCost, nextEnd in graph[end]:
+            for nextCost, nextEnd in graph[end]:  # 방금 추가한 노드의 인접 노드에 대하여 반복
                 if visited[nextEnd] == False:
-                    heapq.heappush(edges, (nextCost, end, nextEnd))
-    
-    return mst
+                    heapq.heappush(edge, (nextCost, end, nextEnd))
 
-prim(graph, 1)
-print(distance)
+    return distance
+
+print(prim(graph, 1))
