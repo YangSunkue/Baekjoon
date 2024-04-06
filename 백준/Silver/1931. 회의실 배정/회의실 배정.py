dfa182315234b0607384917be6aeb0ef5e1bb3fe
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-04-06 19:50:01 +0900

    [Silver I] Title: 회의실 배정, Time: 268 ms, Memory: 59068 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,21 +1,21 @@
 import sys
+
 input = sys.stdin.readline
 
-N = int(input())  # 회의 개수
+N = int(input())
+talkList = []
+for _ in range(N):
+    talkList.append(list(map(int, input().split())))
 
-time = [[0] * 2 for _ in range(N)]  # 회의 시작/끝 시간
-for i in range(N):
-    s, e = map(int, input().split())
-    time[i][0] = s
-    time[i][1] = e
+talkList.sort(key = lambda x: (x[1], x[0]))
 
-time.sort(key = lambda x: (x[1], x[0]))
-
-cnt = 1  # 최대 회의 개수
-end_time = time[0][1]  # 첫번째 회의 끝나는 시간
-for i in range(1, N):
-    if time[i][0] >= end_time:
+diff = talkList[0][1]
+cnt = 1
+for i in range(1, N):  # 회의 하나씩 반복
+    if diff <= talkList[i][0]:
         cnt += 1
-        end_time = time[i][1]
+        diff = talkList[i][1]
 
 print(cnt)
+
+# [[0, 6], [1, 4], [2, 13], [3, 5], [3, 8], [5, 7], [5, 9], [6, 10], [8, 11], [8, 12], [12, 14]]
