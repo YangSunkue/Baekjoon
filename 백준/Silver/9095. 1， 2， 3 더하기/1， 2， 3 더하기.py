ec38665d6ddc1c7e95e13bb2417e8f0624936c3b
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-08-22 02:28:11 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -3,8 +3,6 @@ input = sys.stdin.readline
 
 N = int(input())
 
-# num-1, num-2, num-3  경우의 수들에서 각각 1, 2, 3을 한번씩만 더해주면 num의 경우의 수가 된다.
-# 즉 num-1, num-2, num-3 경우의 수를 모두 합치면 된다.
 def plus(num):
     if num == 1:
         return 1
@@ -12,7 +10,6 @@ def plus(num):
         return 2
     elif num == 3:
         return 4
-    
     else:
         return plus(num-1) + plus(num-2) + plus(num-3)
 
