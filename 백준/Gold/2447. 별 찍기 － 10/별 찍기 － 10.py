8fe5d2b5ddfd3fcc2c7338693bc47857ec4e4c39
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-09-30 12:30:43 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 60 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -3,32 +3,36 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-
-start = ['***', '* *', '***']  # N = 3일 경우 별 초기화
+start = ['***', '* *', '***'] # N이 3일 경우의 초기 모양
 
 def divideAndConquer(num, star):
-    
-    # 종료조건
+
+    # 종료 조건
     if num == N:
         return star
     
-    # 별 모양을 늘린다
-    tmpStar = []
-    # 아래로 3배
+    """
+    이 반복문은 기존 star를 그대로 출력하는 것을 의미한다.
+    따라서 3배 곱하여 append해주면 옆으로 3배 늘리는 것이 된다.
     for i in range(num):
-        tmpStar.append(star[i] * 3) # 옆으로 3배
+        print(star[i])
+    """
 
-    # 아래로 3배
+    tmpStar = []
+    # 옆으로 3배 늘리는 작업을 3번 반복한다
     for i in range(num):
-        tmpStar.append(star[i] + ' ' * num + star[i]) # 옆으로 3배
-        
-    # 아래로 3배
+        tmpStar.append(star[i] * 3)
+    
+    # 가운데 부분은 공백으로 만든다
     for i in range(num):
-        tmpStar.append(star[i] * 3) # 옆으로 3배
+        tmpStar.append(star[i] + ' ' * num + star[i])
+    
+    for i in range(num):
+        tmpStar.append(star[i] * 3)
     
     return divideAndConquer(num * 3, tmpStar)
 
-
+# 정답 출력
 result = divideAndConquer(3, start)
 for re in result:
     print(re)
