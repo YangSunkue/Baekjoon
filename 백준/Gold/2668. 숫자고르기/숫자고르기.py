baee826f3473d971925a4db3a753164f43bbcbc3
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-10-05 14:47:32 +0900

    [Gold V] Title: 숫자고르기, Time: 28 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/2668. 숫자고르기/숫자고르기.py
+++ 백준/Gold/2668. 숫자고르기/숫자고르기.py
@@ -6,27 +6,31 @@ input = sys.stdin.readline
 N = int(input())
 graph = [[] for _ in range(N + 1)]
 for i in range(1, N + 1):
-    graph[i].append(int(input())) # 역방향 그래프
+    graph[i].append(int(input()))
 
-def DFS(node, start):
+def DFS(cur, start, tmpPath): # 현재노드, 시작노드
 
-    visited[node] = True
+    visited[cur] = True
+    tmpPath.append(cur)
 
-    for nextNode in graph[node]:
-        if nextNode == start: # 사이클 발견
-            cycle.add(start)
-            return # 한 노드당 사이클은 하나뿐이다
-        
-        if not visited[nextNode]:
-            DFS(nextNode, start)
+    for edge in graph[cur]:
+        if edge in path: continue # 이미 싸이클에 포함된 노드일 경우 continue
 
-cycle = set()
-for start in range(1, N + 1):
-    if start not in cycle:
+        if not visited[edge]:
+            DFS(edge, start, tmpPath)
+
+        elif edge == start:
+            for t in tmpPath:
+                path.append(t)
+
+# 한 노드가 하나의 사이클에만 포함된다면 path를 저장하고 path에 저장된 정점은 visited와 동일하게 취급해야 함
+path = []
+for i in range(1, N + 1):
+    if i not in path:
         visited = [False] * (N + 1)
-        DFS(start, start)
+        DFS(i, i, [])
 
-result = sorted(list(cycle))
-print(len(result))
-for re in result:
-    print(re)
+path.sort()
+print(len(path))
+for pa in path:
+    print(pa)
