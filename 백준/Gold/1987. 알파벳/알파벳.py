cb20a05252659d8a9b23a7de9a0503dc2e529f60
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-10-06 17:06:08 +0900

    [Gold IV] Title: 알파벳, Time: 7292 ms, Memory: 145368 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,44 @@
+# DFS, 백트래킹
+import sys
+input = sys.stdin.readline
+
+R, C = map(int, input().split()) # 행, 열
+board = []
+for _ in range(R):
+    board.append(list(input().strip()))
+
+# 상/하/좌/우로 가도록 한다
+# 인접노드는 어차피 상하좌우로 동일해서 인접리스트 따로 안만들어도 됨
+directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+
+visited = [[False] * C for _ in range(R)]
+visited[0][0] = True
+path = set() # 집합으로 불필요한 중복 알파벳이 들어가는 것을 방지
+result = 1
+def backTracking(x, y, depth):
+
+    global result
+    path.add(board[x][y]) # 경로에 알파벳 추가
+    result = max(result, depth) # 최댓값 갱신
+
+    # 4방향 체크
+    for di in directions:
+        nx, ny = x + di[0], y + di[1]
+        
+        # 갈수있으면 재귀
+        if goingNoGoing(nx, ny):
+            visited[nx][ny] = True
+            backTracking(nx, ny, depth + 1)
+            visited[nx][ny] = False
+    
+    path.remove(board[x][y]) # 돌아가기 전 경로에서 알파벳 빼주기
+
+
+def goingNoGoing(nx, ny):
+    # board안에 있고, 겹치는 알파벳 아니고, 아직 방문 안했을 경우 True
+    if 0 <= nx < R and 0 <= ny < C and board[nx][ny] not in path and not visited[nx][ny]:
+        return True
+    return False
+
+backTracking(0, 0, 1)
+print(result)
