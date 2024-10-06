d365d41580bcde5c739b958c374f9443306ee796
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-10-06 17:18:45 +0900

    [Gold IV] Title: 알파벳, Time: 7136 ms, Memory: 145344 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -2,19 +2,20 @@
 import sys
 input = sys.stdin.readline
 
-R, C = map(int, input().split()) # 행, 열
+# 입력받기
+R, C = map(int, input().split())
 board = []
 for _ in range(R):
     board.append(list(input().strip()))
 
-# 상/하/좌/우로 가도록 한다
-# 인접노드는 어차피 상하좌우로 동일해서 인접리스트 따로 안만들어도 됨
+# 사전준비
 directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]
-
 visited = [[False] * C for _ in range(R)]
 visited[0][0] = True
 path = set() # 집합으로 불필요한 중복 알파벳이 들어가는 것을 방지
 result = 1
+
+# DFS로 모든 경로의 깊이를 탐색후 최대 깊이 갱신하는 함수
 def backTracking(x, y, depth):
 
     global result
@@ -33,12 +34,13 @@ def backTracking(x, y, depth):
     
     path.remove(board[x][y]) # 돌아가기 전 경로에서 알파벳 빼주기
 
-
+# 다음 좌표가 유효한지 확인하는 함수
 def goingNoGoing(nx, ny):
     # board안에 있고, 겹치는 알파벳 아니고, 아직 방문 안했을 경우 True
     if 0 <= nx < R and 0 <= ny < C and board[nx][ny] not in path and not visited[nx][ny]:
         return True
     return False
 
+# 정답 출력
 backTracking(0, 0, 1)
 print(result)
