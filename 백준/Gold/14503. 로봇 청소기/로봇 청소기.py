9c7341f8c72643cf5e0e316971636a94dccf431c
양선규 <160471618+YangSunkue@users.noreply.github.com>
2024-10-07 15:13:14 +0900

    [Gold V] Title: 로봇 청소기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.py
@@ -7,29 +7,28 @@ N, M = map(int, input().split())
 x, y, d = map(int, input().split())
 room = [list(map(int, input().split())) for _ in range(N)]
 
-
-# 북 동 남 서
+# 0, 1, 2, 3 -> 북 동 남 서
 dx = [-1, 0, 1, 0]
 dy = [0, 1, 0, -1]
 
+# 청소 시작
 room[x][y] = 2
 result = 1
 def cleaner(x, y, d):
 
     global result
 
-    for _ in range(50**3): # 혹시 모를 무한루프 방지
+    # 무한루프 방지를 위한 for문
+    for _ in range(50**3):
+        clean = False # 청소여부
 
-        clean = False # 청소했는지 여부
-
-        # 미청소 칸 탐색
+        # 4방향 탐색
         for _ in range(4):
-            # 반시계 회전 후 다음 경로 지정
             d = (d + 3) % 4
             nx = x + dx[d]
             ny = y + dy[d]
 
-            # 다음 경로가 미청소 빈칸이면 결과 및 좌표 갱신
+            # 미청소 구역일 경우 전진
             if room[nx][ny] == 0:
                 clean = True
                 room[nx][ny] = 2
@@ -37,16 +36,16 @@ def cleaner(x, y, d):
                 x, y = nx, ny
                 break
         
-        # 4방향 모두 갈 곳이 없을 경우 후진 ( 진행방향의 반대 )
-        # 순환적으로 인덱스 2 증가시키면 반대 방향임
+        # 4방향 갈곳이 없으면 후진 ( 방향 유지 )
         if not clean:
-            tmp = (d + 2) % 4
-            nx = x + dx[tmp]
-            ny = y + dy[tmp]
-
-            # 후진시 벽일 경우 종료
-            if room[nx][ny] == 1:
-                return
+            tmpDir = (d + 2) % 4
+            nx = x + dx[tmpDir]
+            ny = y + dy[tmpDir]
+        
+        # 후진했는데 벽이라면 종료
+        if room[nx][ny] == 1:
+            return
+        else:
             x, y = nx, ny
 
 # 결과 출력
