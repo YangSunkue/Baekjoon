c748d809a8d2f71d41c280c72aca295ae8083194
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-01-20 12:04:39 +0900

    [Silver IV] Title: 국영수, Time: 444 ms, Memory: 155708 KB -BaekjoonHub

--- 백준/Silver/10825. 국영수/국영수.py
+++ 백준/Silver/10825. 국영수/국영수.py
@@ -1,16 +1,9 @@
-# 실버4, 정렬
+# 실버4 -> 정렬
 
 score = []
 for i in range(int(input())):
-    data = list(input().split())
-    # 점수를 int로 변환
-    data[1], data[2], data[3] = int(data[1]), int(data[2]), int(data[3])
+    score.append(list(input().split()))
+sorted_score = sorted(score, key=lambda x: (-int(x[1]), int(x[2]), -int(x[3]), x[0]))
 
-    score.append(data)
-
-# 다중 조건 정렬
-sorted_score = sorted(score, key=lambda x: (-x[1], x[2], -x[3], x[0]))
-
-# 정렬된 이름 출력
 for name in sorted_score:
     print(name[0])
