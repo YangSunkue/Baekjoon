30d9e3d5c821956f1db7f905777c93bca56b4eee
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-02-07 19:00:08 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/6603. 로또/로또.py
@@ -0,0 +1,23 @@
+# 실버2 -> 백트래킹
+import sys
+input = sys.stdin.readline
+
+def back_tracking(depth, start, result):
+
+    if depth == 6:
+        print(*result)
+        return
+    
+    for i in range(start, len(S)):
+        result.append(S[i])
+        back_tracking(depth + 1, i + 1, result)
+        result.pop()
+
+
+while True:
+
+    K, *S = map(int, input().split())
+    if K == 0: break
+
+    back_tracking(0, 0, [])
+    print()
