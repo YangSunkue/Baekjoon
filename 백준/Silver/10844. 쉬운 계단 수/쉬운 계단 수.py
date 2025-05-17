0bef15ecca8cca014fec871b9803dde60e10ac3e
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-05-17 20:25:10 +0900

    [Silver I] Title: 쉬운 계단 수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
+++ 백준/Silver/10844. 쉬운 계단 수/쉬운 계단 수.py
@@ -1,15 +1,14 @@
-# 다이나믹 프로그래밍
 import sys
 input = sys.stdin.readline
 
 N = int(input())
-dp = [[0 for _ in range(12)] for _ in range(N + 1)]
-for i in range(2, len(dp[1]) - 1):
-    dp[1][i] = 1
+dp = [[0 for _ in range(12)] for _ in range(N)]
+for i in range(2, len(dp[0]) - 1):
+    dp[0][i] = 1
 
-for i in range(2, N + 1):
+for i in range(1, N):
     for j in range(1, 11):
-
+        
         dp[i][j] = dp[i-1][j-1] + dp[i-1][j+1]
 
-print(sum(dp[N]) % int(1e9))
+print(sum(dp[N-1]) % int(1e9))
