61b1a4f3fb358cc9078c765cf87d121c226f2678
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-05-17 13:34:21 +0900

    [Silver II] Title: 종이의 개수, Time: 3228 ms, Memory: 70320 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,49 +1,41 @@
-# 실버2 -> 분할 정복, 재귀
-"""
-N : 1 또는 3의 k승
-
--1, 0, 1 로 이루어진 N * N 행렬
-하나의 수로 이루어져 있지 않다면 9개로 분할
-분할을 마친 후, 각 숫자로 이루어진 종이가 몇개인지 출력
-"""
-
 import sys
 input = sys.stdin.readline
 
+# 분할 함수 : 같은 수로 되어있는지 체크, 아닐경우 분할, 맞다면 -1/0/1 카운트 올리기
+# 같은 수로만 되어 있지 않다면 9분할
+
 N = int(input())
-table = [list(map(int, input().split())) for _ in range(N)]
+data = [list(map(int, input().split())) for _ in range(N)]
 
 minus, zero, plus = 0, 0, 0
-def divide(x, y, n):
+
+def divide_and_conquer(x, y, length):
 
     global minus, zero, plus
 
-    prev = table[x][y] # 기준이 될 숫자 설정
-    for i in range(x, x + n):
-        for j in range(y, y + n):
-            current = table[i][j]
-            if prev != current: # 단일 숫자가 아닐 경우 9개 분할
-                ne = n // 3
-                divide(x, y, ne)
-                divide(x + ne, y, ne)
-                divide(x + ne*2, y, ne)
-                divide(x, y + ne, ne)
-                divide(x, y + ne*2, ne)
-                divide(x + ne, y + ne, ne)
-                divide(x + ne, y + ne*2, ne)
-                divide(x + ne*2, y + ne, ne)
-                divide(x + ne*2, y + ne*2, ne)
+    prev = data[x][y]
+    for i in range(x, x + length):
+        for j in range(y, y + length):
+            if data[i][j] != prev:
+                
+                le = length // 3
 
+                divide_and_conquer(x, y, le)
+                divide_and_conquer(x + le, y, le)
+                divide_and_conquer(x + le*2, y, le)
+                divide_and_conquer(x, y + le, le)
+                divide_and_conquer(x + le, y + le, le)
+                divide_and_conquer(x + le*2, y + le, le)
+                divide_and_conquer(x, y + le*2, le)
+                divide_and_conquer(x + le, y + le*2, le)
+                divide_and_conquer(x + le*2, y + le*2, le)
                 return
-
-    # 분할되지 않은 경우, 맞는 count 올리고 return
-    if prev == -1:
-        minus += 1
-    elif prev == 0:
-        zero += 1
+    
+    if prev == -1: minus += 1
+    elif prev == 0: zero += 1
     else: plus += 1
 
-divide(0, 0, N)
+divide_and_conquer(0, 0, N)
 print(minus)
 print(zero)
 print(plus)
