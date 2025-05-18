bc0ecb1b32447a268b7842869532b8856fb6c193
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-05-18 12:54:02 +0900

    [Gold IV] Title: 연구소, Time: 2040 ms, Memory: 123628 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -3,23 +3,18 @@ import copy
 import sys
 input = sys.stdin.readline
 
-# 0: 빈칸, 1: 벽, 2: 바이러스
-# 벽 3개를 세우는는 안전 영역 최댓값 구하기
-
-# 백트래킹 : 벽 세우기
-# 벽 3개 되면 BFS실행
-
-# BFS : 바이러스 위치 큐에 넣고 while문 실행, 안전 영역 개수 세서 갱신하기
-
 N, M = map(int, input().split())
 table = [list(map(int, input().split())) for _ in range(N)]
 
-# 바이러스 위치 찾기
 virus = []
 for x in range(N):
     for y in range(M):
         if table[x][y] == 2:
             virus.append([x, y])
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+result = 0
 
 def create_wall(wall):
 
@@ -33,15 +28,13 @@ def create_wall(wall):
                 table[x][y] = 1
                 create_wall(wall + 1)
                 table[x][y] = 0
-
-dx = [-1, 1, 0 ,0]
-dy = [0, 0, -1, 1]
-result = 0
+    
 def bfs():
-    
+
     global result
-    
+
     temp_table = copy.deepcopy(table)
+
     queue = deque([])
     for v in virus:
         queue.append(v)
