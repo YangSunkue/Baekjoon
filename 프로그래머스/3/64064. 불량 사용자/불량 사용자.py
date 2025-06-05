ac7591f7488879bb482cdee30d4fa793039c4690
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-06-05 18:49:32 +0900

    [level 3] Title: 불량 사용자, Time: 126.04 ms, Memory: 9.16 MB -BaekjoonHub

--- 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
+++ 프로그래머스/3/64064. 불량 사용자/불량 사용자.py
@@ -8,11 +8,12 @@ def solution(user_id, banned_id):
             if banned_user[i] != '*' and banned_user[i] != user[i]:
                 return False
         return True
+        
     
     def back_tracking(depth, selected):
         
         if depth == len(banned_id):
-            result_set.add(frozenset(selected))  # 순서가 다른것도 중복으로 인식하도록 frozenset 사용
+            result_set.add(frozenset(selected))
             return
         
         for user in user_id:
@@ -20,11 +21,8 @@ def solution(user_id, banned_id):
                 selected.add(user)
                 back_tracking(depth + 1, selected)
                 selected.remove(user)
-            
-            
-            
-    # set으로 바꿔 중복 제거 (순서만 다른 조합들)
+    
     result_set = set()
-    back_tracking(0, set()) # not in 연산 효율성을 위해 set 선택(key로 접근하는 O(1)), 리스트는 O(N)
+    back_tracking(0, set())
     
     return len(result_set)
