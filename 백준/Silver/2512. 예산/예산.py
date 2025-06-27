49ab55972aa4cd1fdefc0472dea0ef0ef61cb6bf
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-06-27 22:59:44 +0900

    [Silver II] Title: 예산, Time: 36 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.py
+++ 백준/Silver/2512. 예산/예산.py
@@ -1,29 +1,74 @@
 import sys
 input = sys.stdin.readline
 
+"""
+1. 모든 요청을 줄 수 있다면 다 준다
+2. 상한액을 정해서, 상한액 이상인 요청엔 상한액만 준다. 상한액 이하의 요청은 그대로 준다
+
+이것도 이분탐색..?
+"""
+
 N = int(input())
 reqs = list(map(int, input().split()))
 budget = int(input())
 
-reqs.sort()
-left = 1
-right = max(reqs)
-answer = 0
+def calc(limit, arr):
+    """
+    상한가를 고려해, 지급해야 할 금액 계산
+    limit: 상한가
+    """
+    left = 0
+    right = len(arr) - 1
 
-while left <= right:
-    mid = (left + right) // 2
-    
-    total = 0
-    for req in reqs:
-        if req > mid:
-            total += mid
+    # limit_idx 포함한 오른쪽은 전부 상한가다
+    # 일단 모든 요소가 상한가보다 작다고 가정
+    limit_idx = len(arr)
+
+    while left <= right:
+
+        mid = (left + right) // 2
+
+        if arr[mid] >= limit:
+            right = mid - 1
+            limit_idx = mid
+        
         else:
-            total += req
+            left = mid + 1
     
-    if total <= budget:
-        answer = mid 
-        left = mid + 1
-    else:
-        right = mid - 1
+    left_sum = sum(arr[:limit_idx])
+    right_sum = limit * len(arr[limit_idx:])
 
-print(answer)
+    return left_sum + right_sum
+
+
+
+if sum(reqs) <= budget:
+
+    """예산이 충분할 경우"""
+    print(max(reqs))
+
+else:
+
+    """예산이 부족할 경우, 상한가 기준 이분탐색"""
+    reqs.sort()
+    left = 1
+    right = reqs[-1] - 1
+
+    while left <= right:
+
+        # mid는 현재 상한가
+        mid = (left + right) // 2
+        total = calc(mid, reqs)
+
+        # 예산이 딱 맞는다면 바로 리턴 (최적 상한가)
+        if total == budget:
+            right = mid
+            break
+
+        elif total > budget:
+            right = mid - 1
+
+        else:
+            left = mid + 1
+    
+    print(right)
