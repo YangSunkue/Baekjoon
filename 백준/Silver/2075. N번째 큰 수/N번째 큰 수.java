45b95880b7e1791f6205b4a6b1d785168c139af8
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-07-24 18:57:10 +0900

    [Silver III] Title: N번째 큰 수, Time: 796 ms, Memory: 274988 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -8,22 +8,25 @@ public class Main {
 		StringTokenizer st;
 		
 		int N = Integer.parseInt(br.readLine());
-		int[] table = new int[N*N];
 		
-		/** 숫자를 하나의 배열로 전부 입력받기 */
-		int idx = 0;
+		/** 우선순위 큐 사용 */
+		PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			for (int j = 0; j < N; j++) {
-				table[idx] = Integer.parseInt(st.nextToken());
-				idx++;
+				int value = Integer.parseInt(st.nextToken());
+				maxPq.offer(value);
 			}
 		}
-		Arrays.sort(table);  // 오름차순 정렬
 		
-		/** N번째로 큰 수 출력 */
-		System.out.println(table[table.length - N]);
+		/** N번째 큰 수 추출 */
+		int result = -1;
+		for (int i = 0; i < N; i++) {
+			result = maxPq.poll();
+		}
+		
+		System.out.println(result);
 	}
 
 }
