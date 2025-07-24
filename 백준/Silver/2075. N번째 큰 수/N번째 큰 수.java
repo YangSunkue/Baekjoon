69ebb310d69b7442deecd91ded60134dc1848001
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-07-24 19:37:32 +0900

    [Silver III] Title: N번째 큰 수, Time: 780 ms, Memory: 213124 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -9,24 +9,33 @@ public class Main {
 		
 		int N = Integer.parseInt(br.readLine());
 		
-		/** 우선순위 큐 사용 */
-		PriorityQueue<Integer> maxPq = new PriorityQueue<>(Collections.reverseOrder());
+		/**
+		 * 우선순위 큐 사용 (최소 힙)
+		 * 
+		 * - N^2개 숫자 중 가장 큰 N개의 요소만 힙에 저장
+		 * - 힙에 저장된 값 중 가장 작은 값이 N번째 큰 수
+		 */
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
 		for (int i = 0; i < N; i++) {
 			st = new StringTokenizer(br.readLine());
 			
 			for (int j = 0; j < N; j++) {
 				int value = Integer.parseInt(st.nextToken());
-				maxPq.offer(value);
+				
+				// N개까지 우선적으로 저장
+				if (pq.size() < N) {
+					pq.offer(value);
+				}
+				// 큐 최소값과 value 비교해서 큰 값을 저장
+				else if (pq.peek() < value) {
+					pq.poll();
+					pq.offer(value);
+				}
 			}
 		}
 		
-		/** N번째 큰 수 추출 */
-		int result = -1;
-		for (int i = 0; i < N; i++) {
-			result = maxPq.poll();
-		}
-		
-		System.out.println(result);
+		/** 가장 큰 N개의 수 중, 가장 작은 값이 N번째로 큰 값이다 */
+		System.out.println(pq.peek());
 	}
 
 }
