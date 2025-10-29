fa701034a3c09fcc203eb9f719e774e74436a1de
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-10-29 12:34:13 +0900

    [level 2] Title: 양궁대회, Time: 1.70 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/2/92342. 양궁대회/양궁대회.py
+++ 프로그래머스/2/92342. 양궁대회/양궁대회.py
@@ -1,52 +1,51 @@
 def solution(n, info):
-
-    def calc_diff_score(info2):
-        apeach = 0
+    
+    def select_result(result_list):
+        result_list.sort(key=lambda x: x[::-1], reverse=True)
+        return result_list[0]
+    
+    def calculate_diff(info2):
         lion = 0
-
+        apeach = 0
+        
         for i in range(11):
             if (info[i], info2[i]) == (0, 0):
                 continue
-
-            if info[i] < info2[i]:
+            
+            if info2[i] > info[i]:
                 lion += 10 - i
             else:
                 apeach += 10 - i
+        
         return lion - apeach
-    
-    def select_result(result_list):
-        result_list.sort(key = lambda x: x[::-1], reverse=True)
-        return result_list[0]
+        
     
     result_list = []
     final_diff = 0
-    def back_tracking(score_idx, arrow_used, info2):
-        nonlocal final_diff, result_list
-
-        # 10 ~ 1점까지 결정 완료
-        if score_idx == 10:
-            info2[10] = n - arrow_used
-            diff = calc_diff_score(info2)
-
-            if diff > final_diff:
+    def back_tracking(arrow_idx, used_arrow, info2):
+        
+        nonlocal result_list, final_diff
+        
+        if arrow_idx == 10:
+            info2[arrow_idx] = n - used_arrow
+            
+            diff = calculate_diff(info2)
+            if final_diff < diff:
                 final_diff = diff
                 result_list = [info2[:]]
-            elif diff == final_diff:
+            elif final_diff == diff:
                 result_list.append(info2[:])
             
-            # 원상복구
-            info2[10] = 0
+            info2[arrow_idx] = 0
             return
         
-        # 현재 점수 가져가기 (어피치보다 1발 더 필요)
-        needed = info[score_idx] + 1
-        if (n - arrow_used) >= needed:
-            info2[score_idx] = needed
-            back_tracking(score_idx + 1, arrow_used + needed, info2)
-            info2[score_idx] = 0
+        needed = info[arrow_idx] + 1
+        if (n - used_arrow) >= needed:
+            info2[arrow_idx] = needed
+            back_tracking(arrow_idx + 1, used_arrow + needed, info2)
+            info2[arrow_idx] = 0
         
-        # 점수 포기
-        back_tracking(score_idx + 1, arrow_used, info2)
+        back_tracking(arrow_idx + 1, used_arrow, info2)
     
     back_tracking(0, 0, [0] * 11)
     
