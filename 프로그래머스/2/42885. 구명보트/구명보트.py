b6f1ad4fa976ea1213ebdc6f0abb55f4b65c7bd4
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-02 18:42:53 +0900

    [level 2] Title: 구명보트, Time: 8.81 ms, Memory: 9.54 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,8 +1,5 @@
 def solution(people, limit):
     
-    """
-    가장 무거운 사람과 가장 가벼운 사람을 함께 태운다
-    """
     people.sort()
     left, right = 0, len(people) - 1
     
@@ -10,7 +7,7 @@ def solution(people, limit):
     while left <= right:
         if people[left] + people[right] <= limit:
             left += 1
-            
+        
         right -= 1
         result += 1
     
