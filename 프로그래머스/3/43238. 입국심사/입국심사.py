40743621be864fa4791a7178c6d27d3af3274539
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-02 19:27:08 +0900

    [level 3] Title: 입국심사, Time: 570.29 ms, Memory: 17.5 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.py
+++ 프로그래머스/3/43238. 입국심사/입국심사.py
@@ -1,25 +1,16 @@
 def solution(n, times):
     
-    """
-        시간 기준 이분탐색
-    
-        -> 특정 시간에 대해서, 몇 명의 사람을 심사할 수 있는가?
-    """
-    
-    def calculate_people(time):
-        return sum([time // t for t in times])
-    
-    start = 0
-    end = max(times) * n
+    left = 0
+    right = max(times) * n
     result = 0
-    while start <= end:
+    while left <= right:
         
-        mid = (start + end) // 2
+        mid = (left + right) // 2
         
-        if calculate_people(mid) >= n:
+        if sum([mid // time for time in times]) >= n:
             result = mid
-            end = mid - 1
+            right = mid - 1
         else:
-            start = mid + 1
+            left = mid + 1
     
     return result
