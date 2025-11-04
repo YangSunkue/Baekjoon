818a53d08e38423681de83ba4cc3dc6665a2d826
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-04 16:52:00 +0900

    [level 2] Title: 롤케이크 자르기, Time: 881.11 ms, Memory: 50.6 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.py
@@ -1,26 +1,27 @@
 from collections import defaultdict
 
 def solution(topping):
-    
-    """길이 1인 경우도 고려"""
+
     le = len(topping)
-    
+
     a_dict = defaultdict(int)
     b_dict = defaultdict(int)
-    
-    a_dict[topping[0]] += 1
+
+    a_dict[topping[0]] = 1
     for i in range(1, le):
         b_dict[topping[i]] += 1
     
-    result = 0
     a_cnt = 1
     b_cnt = len(b_dict)
-    
-    if a_cnt == b_cnt: result += 1
+    if a_cnt == b_cnt:
+        result = 1
+    else:
+        result = 0
     
     for i in range(1, le - 1):
+
         t = topping[i]
-        
+
         if a_dict[t] == 0:
             a_cnt += 1
         if b_dict[t] == 1:
@@ -28,7 +29,7 @@ def solution(topping):
         
         a_dict[t] += 1
         b_dict[t] -= 1
-        
+
         if a_cnt == b_cnt:
             result += 1
     
