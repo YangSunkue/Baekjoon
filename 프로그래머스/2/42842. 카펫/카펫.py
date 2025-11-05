9821531425342570211733055a5aaf7da064443c
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-05 19:19:35 +0900

    [level 2] Title: 카펫, Time: 0.08 ms, Memory: 9.21 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.py
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -3,7 +3,7 @@ def solution(brown, yellow):
     total = brown + yellow
     
     # 세로 길이를 3부터 1씩 증가시킨다
-    for height in range(3, int(total ** 0.5) + 1):
+    for height in range(3, total):
         
         # 사각형이 가능할 경우
         if total % height == 0:
