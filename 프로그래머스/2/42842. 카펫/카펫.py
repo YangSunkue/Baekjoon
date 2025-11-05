e1193bd3733aad99ccd898720aefd5cea788992f
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-05 19:13:33 +0900

    [level 2] Title: 카펫, Time: 0.11 ms, Memory: 9.25 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.py
@@ -0,0 +1,14 @@
+def solution(brown, yellow):
+    
+    total = brown + yellow
+    
+    # 세로 길이를 3부터 1씩 증가시킨다
+    for height in range(3, int(total ** 0.5) + 1):
+        
+        # 사각형이 가능할 경우
+        if total % height == 0:
+            width = total // height
+            
+            # 카펫 개수가 맞는 경우
+            if (height + width) * 2 - 4 == brown:
+                return [width, height]
