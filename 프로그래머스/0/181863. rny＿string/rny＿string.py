0a5c4444a11335ad7f9e60061181f2c23f964133
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-08 20:50:09 +0900

    [level 0] Title: rny_string, Time: 0.02 ms, Memory: 8.98 MB -BaekjoonHub

---
+++ 프로그래머스/0/181863. rny＿string/rny＿string.py
@@ -0,0 +1,10 @@
+def solution(rny_string):
+    
+    result = ''
+    for s in rny_string:
+        if s == 'm':
+            result += 'rn'
+        else:
+            result += s
+    
+    return result
