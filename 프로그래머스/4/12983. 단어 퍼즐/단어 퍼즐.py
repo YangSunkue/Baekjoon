1ba23ad4bdca066fd867520c5aa7127378aa1da9
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-17 15:32:10 +0900

    [level 4] Title: 단어 퍼즐, Time: 29.98 ms, Memory: 9.64 MB -BaekjoonHub

--- 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.py
+++ 프로그래머스/4/12983. 단어 퍼즐/단어 퍼즐.py
@@ -1,17 +1,13 @@
 def solution(strs, t):
-    
-    """
-    dp[i]: 0~i 까지 필요한 단어 수
-    """
-    
+
     n = len(t)
 
     dp = [float('inf') for _ in range(n + 1)]
     dp[0] = 0
-    
-    strs = set(strs)
+
     sizes = set(len(s) for s in strs)
-    
+    strs = set(strs)
+
     for i in range(1, n + 1):
         for size in sizes:
             if (i - size) >= 0 and t[i - size : i] in strs:
