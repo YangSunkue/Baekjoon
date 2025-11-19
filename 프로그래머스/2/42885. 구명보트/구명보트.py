3563af8b882d3a3b2fd27411d3dc6d80672f5a26
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-19 11:49:20 +0900

    [level 2] Title: 구명보트, Time: 9.68 ms, Memory: 9.68 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.py
+++ 프로그래머스/2/42885. 구명보트/구명보트.py
@@ -1,13 +1,17 @@
 def solution(people, limit):
     
+    """
+    10 20 30 70 80 80
+    """
+    
     people.sort()
-    left, right = 0, len(people) - 1
     
     result = 0
+    left, right = 0, len(people) - 1
     while left <= right:
         if people[left] + people[right] <= limit:
             left += 1
-        
+            
         right -= 1
         result += 1
     
