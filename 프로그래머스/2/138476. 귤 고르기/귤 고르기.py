88c9d3cb918abb93537ca05d1190869ee36e0da8
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-19 13:34:38 +0900

    [level 2] Title: 귤 고르기, Time: 71.46 ms, Memory: 26.4 MB -BaekjoonHub

--- 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.py
@@ -1,30 +1,19 @@
 from collections import defaultdict
 
 def solution(k, tangerines):
-    
-    """
-    [1, 2, 2, 3, 3, 4, 5, 5]
-    
-    1. 크기별 개수 세기
-    2. k가 될 때까지, 개수가 가장 많은 귤 넣기
-    3. len(set(t)) 구하기
-    """
-    
+
     size_to_num = defaultdict(int)
-    
     for tangerine in tangerines:
         size_to_num[tangerine] += 1
-    size_to_num = defaultdict(int, sorted(size_to_num.items(), key=lambda x: -x[1]))
-    
+    size_to_num = sorted(size_to_num.items(), key=lambda x: -x[1])
+
     t = set()
     count = 0
-    for size in size_to_num:
-        
+    for size, num in size_to_num:
         t.add(size)
-        
-        if count + size_to_num[size] < k:
-            count += size_to_num[size]
-            
+
+        if count + num < k:
+            count += num
             continue
         else:
             break
