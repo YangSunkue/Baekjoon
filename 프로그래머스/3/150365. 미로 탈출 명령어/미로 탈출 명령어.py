e633809b294fea623792a57b035f854c0631c853
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-20 16:12:10 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 5.21 ms, Memory: 9.13 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,26 +1,14 @@
 def solution(n, m, x, y, r, c, k):
-    
-    """
-    1. 맨해튼 거리 구해서 d가 k 이하인지 확인 -> k초과면 impossible
-    2. d에서 k까지 남은 이동횟수 짝수인지 확인 -> 홀수면 impossible
-    
-    3. 탐색시작, dlru 순서로 이동 시도 및 1,2 검증
-    
-    좌표검증 + 좌표 -1 
-    """
-    x -= 1
-    y -= 1
-    r -= 1
-    c -= 1
-    
+
     def get_manhatten(x1, y1, x2, y2):
         return abs(x1 - x2) + abs(y1 - y2)
     
     def can_go(x, y):
         return 0 <= x < n and 0 <= y < m
     
+    x, y, r, c = x - 1, y - 1, r - 1, c - 1
     directions = {'d': (1, 0), 'l': (0, -1), 'r': (0, 1), 'u': (-1, 0)}
-    
+
     distance = get_manhatten(x, y, r, c)
     if distance > k or (k - distance) % 2 == 1:
         return 'impossible'
@@ -28,22 +16,22 @@ def solution(n, m, x, y, r, c, k):
     step = 0
     result = ''
     while step < k:
-        
+
         for direction, (dx, dy) in directions.items():
             nx, ny = x + dx, y + dy
-            
+
             if not can_go(nx, ny):
                 continue
-                
+
             distance = get_manhatten(nx, ny, r, c)
-            if distance > k - (step + 1) or (k - (distance + step + 1)) % 2 == 1:
+            if distance > k - (step + 1) or (k - (step + 1) - distance) % 2 == 1:
                 continue
             
-            step += 1
             x, y = nx, ny
+            step += 1
             result += direction
             break
-            
+
         else:
             return 'impossible'
     
