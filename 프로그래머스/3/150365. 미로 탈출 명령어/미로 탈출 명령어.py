d2dbd85c42a838e0d130fa2e75f068346d5acbeb
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-21 14:09:18 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 4.86 ms, Memory: 9.3 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.py
@@ -1,5 +1,5 @@
 def solution(n, m, x, y, r, c, k):
-
+    
     def get_manhatten(x1, y1, x2, y2):
         return abs(x1 - x2) + abs(y1 - y2)
     
@@ -7,8 +7,9 @@ def solution(n, m, x, y, r, c, k):
         return 0 <= x < n and 0 <= y < m
     
     x, y, r, c = x - 1, y - 1, r - 1, c - 1
+    
     directions = {'d': (1, 0), 'l': (0, -1), 'r': (0, 1), 'u': (-1, 0)}
-
+    
     distance = get_manhatten(x, y, r, c)
     if distance > k or (k - distance) % 2 == 1:
         return 'impossible'
@@ -16,22 +17,22 @@ def solution(n, m, x, y, r, c, k):
     step = 0
     result = ''
     while step < k:
-
+        
         for direction, (dx, dy) in directions.items():
             nx, ny = x + dx, y + dy
-
+            
             if not can_go(nx, ny):
                 continue
-
+            
             distance = get_manhatten(nx, ny, r, c)
-            if distance > k - (step + 1) or (k - (step + 1) - distance) % 2 == 1:
+            if distance > k - (step + 1) or (k - (distance + step + 1)) % 2 == 1:
                 continue
             
-            x, y = nx, ny
             step += 1
             result += direction
+            x, y = nx, ny
             break
-
+        
         else:
             return 'impossible'
     
