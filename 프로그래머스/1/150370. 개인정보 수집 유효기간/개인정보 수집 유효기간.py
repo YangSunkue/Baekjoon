0ca717428f348d922bca8bea1f958ada9337c587
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-22 16:18:12 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 0.14 ms, Memory: 9.4 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.py
@@ -1,31 +1,24 @@
 def solution(today, terms, privacies):
+
+    def to_days(date: str) -> int:
+        year, month, day = map(int, date.split('.'))
+        return (year * 12 * 28) + (month * 28) + day
     
-    t_year, t_month, t_day = map(int, today.split('.'))
-    
-    term_dict = dict()
-    for term in terms:
-        key, value = term.split(' ')
-        term_dict[key] = int(value)
-    
+    term_dict = {
+        term.split(' ')[0]: int(term.split(' ')[1])
+        for term in terms
+    }
+
+    today_days = to_days(today)
+
     result = []
     for i, privacy in enumerate(privacies):
         date, term = privacy.split(' ')
-        year, month, day = map(int, date.split('.'))
-        
-        # 유효기간(월)
-        validity = term_dict[term]
-        
-        # 년
-        year += (month + validity - 1) // 12
-        
-        # 월
-        month = ((month + validity) - 1) % 12 + 1
-        
-        if year < t_year:
-            result.append(i + 1)
-        elif year == t_year and month < t_month:
-            result.append(i + 1)
-        elif year == t_year and month == t_month and day <= t_day:
+
+        days = to_days(date)
+        expire_days = days + term_dict[term] * 28
+
+        if expire_days <= today_days:
             result.append(i + 1)
     
     return result
