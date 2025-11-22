31d03efe9dad563ea312684bb72d184010508d66
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-22 14:31:54 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 355.44 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.py
@@ -1,13 +1,5 @@
 def solution(cap, n, deliveries, pickups):
     
-    """
-    그리디하게 먼곳부터 배달 및 수거
-    몇 개 가져가느냐는 생각할 필요 없음
-    어디부터 주냐도 생각할 필요 없음
-    
-    -> 가장 먼 곳부터 가되 배달, 수거 무조건 cap개씩 줄이면 됨
-    """
-    
     deliver = 0
     pickup = 0
     result = 0
