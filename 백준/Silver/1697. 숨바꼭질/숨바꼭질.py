36446b7ccc7be789551394631c7e0c7e83f95181
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-26 21:19:22 +0900

    [Silver I] Title: 숨바꼭질, Time: 140 ms, Memory: 36680 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+
+def can_go(pos):
+    return 0 <= pos <= 100000 and not visited[pos]
+
+visited = [False] * 100001
+
+queue = deque([(N, 0)])
+result = None
+while queue:
+    
+    pos, dist = queue.popleft()
+    if pos == K:
+        result = dist
+        break
+
+    if can_go(pos + 1):
+        queue.append((pos + 1, dist + 1))
+        visited[pos + 1] = True
+    if can_go(pos - 1):
+        queue.append((pos - 1, dist + 1))
+        visited[pos - 1] = True
+    if can_go(pos * 2):
+        queue.append((pos * 2, dist + 1))
+        visited[pos * 2] = True
+
+print(result)
