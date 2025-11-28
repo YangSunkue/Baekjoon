9f1177e1209c53bf923a10521d1e38359a75802a
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-11-28 14:43:44 +0900

    [Gold IV] Title: 부분합, Time: 96 ms, Memory: 42168 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.py
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -1,42 +1,19 @@
 import sys
 input = sys.stdin.readline
 
-"""
-슬라이딩 윈도우 + 이분탐색
-"""
-
 N, S = map(int, input().split())
 numbers = list(map(int, input().split()))
 
-def sliding_window(length):
-    """S이상 찾으면 즉시 True 리턴"""
+left = 0
+cur_sum = 0
+result = N + 1
 
-    value = sum(numbers[:length])
-    if value >= S:
-        return True
-    
-    for i in range(length, N):
-        value -= numbers[i - length]
-        value += numbers[i]
+for right in range(N):
+    cur_sum += numbers[right]
 
-        if value >= S:
-            return True
-        
-    return False
+    while cur_sum >= S:
+        result = min(result, right - left + 1)
+        cur_sum -= numbers[left]
+        left += 1
 
-# 이분탐색으로 수열길이 탐색
-left = 1
-right = N
-result = 1_000_000
-while left <= right:
-
-    mid = (left + right) // 2
-
-    if sliding_window(mid):
-        result = min(result, mid)
-        right = mid - 1
-    else:
-        left = mid + 1
-
-if result == 1_000_000: result = 0
-print(result)
+print(0 if result == N + 1 else result)
