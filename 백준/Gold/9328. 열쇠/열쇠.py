807ae231b8f984b8143b24841ae9862bab7d1d93
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-11 16:42:03 +0900

    [Gold I] Title: 열쇠, Time: 200 ms, Memory: 35028 KB -BaekjoonHub

--- 백준/Gold/9328. 열쇠/열쇠.py
+++ 백준/Gold/9328. 열쇠/열쇠.py
@@ -1,41 +1,24 @@
 from collections import deque, defaultdict
 import sys
 input = sys.stdin.readline
-"""
-소문자: 열쇠
-대문자: 문
-
-.   : 빈 공간
-*   : 벽
-$   : 문서
-"""
 def can_go(nx, ny):
     return 0 <= nx < h and 0 <= ny < w and not visited[nx][ny] and board[nx][ny] != '*'
-
 directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 for _ in range(int(input())):
 
-    h, w = map(int, input().split())                    # 높이, 너비
-    board = [list(input().strip()) for _ in range(h)]   # 맵
-    keys = set()                                        # 키
+    h, w = map(int, input().split())
+    board = [list(input().strip()) for _ in range(h)]
+    keys = set()
     for key in input().strip():
-        if key == '0':
-            continue
         keys.add(key.upper())
-
-    """
-    모든 시작점 큐에 넣고 단일 BFS
-    새 열쇠 획득 시, 해당하는 문들 큐에 추가
-    큐에 넣을 때 visited 체크
-    """
-    visited = [[False] * w for _ in range(h)]
-    doors = defaultdict(list)  # {'A': [(2, 3), (6, 4), ...]}
-
+    
     result = 0
+    visited = [[False] * w for _ in range(h)]
     queue = deque([])
+    doors = defaultdict(list)
 
-    # 모든 시작점을 큐에 넣고 시작
+    # 모든 시작점 큐에 넣기
     for x in range(h):
         for y in range(w):
             if (x == 0 or x == h-1 or y == 0 or y == w-1) and can_go(x, y):
@@ -50,14 +33,10 @@ for _ in range(int(input())):
         # 문
         if item.isupper():
             if item not in keys:
-                visited[cx][cy] = False
                 doors[item].append((cx, cy))
+                visited[cx][cy] = False
                 continue
         
-        # 문서
-        if item == '$':
-            result += 1
-
         # 열쇠
         if item.islower():
             new_key = item.upper()
@@ -67,9 +46,13 @@ for _ in range(int(input())):
                 for i, j in doors[new_key]:
                     queue.append((i, j))
                     visited[i][j] = True
-                doors[new_key] = []
+                    doors[new_key] = []
         
-        # 다음 좌표 탐색
+        # 문서
+        if item == '$':
+            result += 1
+        
+        # 4방향 큐에 추가
         for dx, dy in directions:
             nx, ny = cx + dx, cy + dy
             if can_go(nx, ny):
