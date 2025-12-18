c0c89c91f5f5b513369cbf27a1119aa0d42227a7
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-18 14:16:49 +0900

    [Gold I] Title: 계단 수, Time: 204 ms, Memory: 40488 KB -BaekjoonHub

---
+++ 백준/Gold/1562. 계단 수/계단 수.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+MOD = 1_000_000_000
+
+# dp[길이][마지막숫자][비트마스크] = 경우의 수
+dp = [[[0] * 1024 for _ in range(10)] for _ in range(N + 1)]
+
+# 초기값
+for i in range(1, 10):
+    dp[1][i][1 << i] = 1  # i번 비트만 켜진 상태
+
+# DP 채우기
+for i in range(1, N):
+    for j in range(10):
+        for mask in range(1024):
+
+            if dp[i][j][mask] == 0:
+                continue
+
+            if j > 0:
+                new_mask = mask | (1 << (j - 1))
+                dp[i + 1][j - 1][new_mask] = (dp[i + 1][j - 1][new_mask] + dp[i][j][mask]) % MOD
+
+            if j < 9:
+                new_mask = mask | (1 << (j + 1))
+                dp[i + 1][j + 1][new_mask] = (dp[i + 1][j + 1][new_mask] + dp[i][j][mask]) % MOD
+
+result = 0
+for i in range(10):
+    result = (result + dp[N][i][1023]) % MOD
+
+print(result)
