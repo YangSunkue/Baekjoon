2d5465153c5ee25f29724b859fb5650bff7ab701
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-18 11:15:03 +0900

    [Gold III] Title: 음악프로그램, Time: 56 ms, Memory: 34968 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.py
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.py
@@ -3,22 +3,20 @@ import sys
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-pds = []
-for _ in range(m):
-    data = list(map(int, input().split()))
-    pds.append(data[1:])
 
 adj_list = defaultdict(list)
 in_degree = [0] * (n + 1)
-for pd in pds:
-    for i in range(len(pd)):
-        if i + 1 <= len(pd) - 1:
-            adj_list[pd[i]].append(pd[i + 1])
-            in_degree[pd[i + 1]] += 1
+for _ in range(m):
+    data = list(map(int, input().split()))
+    nodes = data[1:]
 
-# 간선이 없는 노드 큐에 넣기
-result = []
+    for i in range(1, len(nodes)):
+        s, e = nodes[i - 1], nodes[i]
+        adj_list[s].append(e)
+        in_degree[e] += 1
+
 queue = deque([])
+result = []
 for i in range(1, n + 1):
     if in_degree[i] == 0:
         queue.append(i)
