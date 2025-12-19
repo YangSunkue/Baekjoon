94e31c432a723a4c3c0327520e7b55d0f58a1443
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-19 16:05:42 +0900

    [Gold III] Title: 텀 프로젝트, Time: 2240 ms, Memory: 59340 KB -BaekjoonHub

--- 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
+++ 백준/Gold/9466. 텀 프로젝트/텀 프로젝트.py
@@ -4,27 +4,27 @@ sys.setrecursionlimit(10**7)
 
 for _ in range(int(input())):
     N = int(input())
-    choice = [0] + list(map(int, input().split()))
+    choices = [0] + list(map(int, input().split()))
 
     visited = [False] * (N + 1)
     finished = [False] * (N + 1)
     team = [False] * (N + 1)
 
-    def dfs(cur):
+    def dfs(node):
 
-        visited[cur] = True
-        next_node = choice[cur]
+        visited[node] = True
+        next_node = choices[node]
 
         if not visited[next_node]:
             dfs(next_node)
         elif not finished[next_node]:
             temp = next_node
-            while temp != cur:
+            while node != temp:
                 team[temp] = True
-                temp = choice[temp]
-            team[cur] = True
-
-        finished[cur] = True
+                temp = choices[temp]
+            team[node] = True
+        
+        finished[node] = True
     
     for node in range(1, N + 1):
         if not visited[node]:
