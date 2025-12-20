fee5148f7763b83822b1da7ab2d953c37571213d
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-20 20:23:05 +0900

    [Platinum V] Title: 히스토그램에서 가장 큰 직사각형, Time: 288 ms, Memory: 48172 KB -BaekjoonHub

--- 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
+++ 백준/Platinum/6549. 히스토그램에서 가장 큰 직사각형/히스토그램에서 가장 큰 직사각형.py
@@ -3,22 +3,20 @@ input = sys.stdin.readline
 
 while True:
     data = list(map(int, input().split()))
-    n = data[0]
-    nodes = data[1:]
-
-    if n == 0:
+    if data[0] == 0:
         break
+    
+    walls = data[1:]
+    walls.append(0)
 
-    stack = []
     max_size = 0
-    nodes.append(0)
-
-    for i, h in enumerate(nodes):
-        while stack and nodes[stack[-1]] > h:
-            height = nodes[stack.pop()]
+    stack = []
+    for i in range(len(walls)):
+        while stack and walls[stack[-1]] > walls[i]:
+            height = walls[stack.pop()]
             width = i if not stack else i - stack[-1] - 1
             max_size = max(max_size, height * width)
 
         stack.append(i)
-    
+
     print(max_size)
