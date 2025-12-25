26e664f4297df6f658685f0c5fad206a77c2a33c
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-25 16:27:49 +0900

    [Gold IV] Title: 팰린드롬?, Time: 2108 ms, Memory: 63888 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.py
@@ -5,18 +5,18 @@ N = int(input())
 sequence = [0] + list(map(int, input().split()))
 
 """dp 테이블 제작"""
-dp = [[0] * (N + 1) for _ in range(N + 1)]  # dp[i][j]: i번째부터 j번째까지가 팰린드롬인지 여부
+dp = [[0] * (N + 1) for _ in range(N + 1)]
 
-# 길이 1은 전부 팰린드롬
+# 길이 1 -> 전부 팰린드롬
 for i in range(1, N + 1):
     dp[i][i] = 1
 
-# 길이 2는 두 수가 같을 경우 팰린드롬
+# 길이 2 -> 두 수가 같으면 팰린드롬
 for i in range(1, N):
     if sequence[i] == sequence[i + 1]:
         dp[i][i + 1] = 1
 
-# 짧은 길이부터 반복문 돌리기 (3 ~ N)
+# 길이 3 이상 -> sequence[i], [j]가 같고 [i + 1][j - 1]이 팰린드롬이면 팰린드롬
 for size in range(3, N + 1):
     for i in range(1, N - (size - 2)):
         j = i + (size - 1)
