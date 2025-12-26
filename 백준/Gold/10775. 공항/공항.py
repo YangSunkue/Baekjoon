90018d0f6cb3bb7c93b808f16c872cd3f4c1732e
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-26 18:06:20 +0900

    [Gold II] Title: 공항, Time: 136 ms, Memory: 41736 KB -BaekjoonHub

--- 백준/Gold/10775. 공항/공항.py
+++ 백준/Gold/10775. 공항/공항.py
@@ -3,31 +3,23 @@ input = sys.stdin.readline
 sys.setrecursionlimit(10**7)
 
 G = int(input())
-"""
-큰 게이트 우선 배정 (수요 적으므로)
-parent 이용한 경로 압축
-"""
-parent = [i for i in range(G + 1)]
-visited = set()
 
 def find(x):
     if parent[x] != x:
         parent[x] = find(parent[x])
     return parent[x]
 
+parent = [i for i in range(G + 1)]
 result = 0
 for _ in range(int(input())):
 
-    plane = int(input())  # plane 이하 게이트에만 도킹 가능
+    plane = int(input())
     available_gate = find(plane)
 
-    if available_gate in visited:
+    if available_gate == 0:
         break
-    
-    result += 1
-    visited.add(available_gate)
 
-    if available_gate - 1 >= 1:
-        parent[available_gate] = available_gate - 1
+    parent[available_gate] -= 1
+    result += 1
 
 print(result)
