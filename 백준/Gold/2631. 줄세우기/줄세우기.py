d878a26142b399e69db3d464e5fb54841849dbc2
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-29 14:48:55 +0900

    [Gold IV] Title: 줄세우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2631. 줄세우기/줄세우기.py
+++ 백준/Gold/2631. 줄세우기/줄세우기.py
@@ -6,11 +6,11 @@ N - LIS 길이
 """
 
 N = int(input())
-kids = [201] + [int(input()) for _ in range(N)]
+kids = [int(input()) for _ in range(N)]
 
-dp = [1] * (N + 1)
-for i in range(2, N + 1):
-    for j in range(1, i):
+dp = [1] * N
+for i in range(1, N):
+    for j in range(i):
         if kids[i] > kids[j]:
             dp[i] = max(dp[i], dp[j] + 1)
 
