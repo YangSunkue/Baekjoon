7cadac6a22659fbd5d6de29133dbcc1b279a1471
양선규 <160471618+YangSunkue@users.noreply.github.com>
2025-12-31 16:44:30 +0900

    [Gold III] Title: 파티, Time: 68 ms, Memory: 36584 KB -BaekjoonHub

--- 백준/Gold/1238. 파티/파티.py
+++ 백준/Gold/1238. 파티/파티.py
@@ -5,61 +5,40 @@ input = sys.stdin.readline
 
 N, M, X = map(int, input().split())  # 노드, 간선, 목적지
 adj_list = defaultdict(list)
+rvs_list = defaultdict(list)
 for _ in range(M):
     s, e, dist = map(int, input().split())
     adj_list[s].append((dist, e))  # 거리, 목적지
+    rvs_list[e].append((dist, s))
 
-to_node = [float('inf')] * (N + 1)  # x에서 각 i번 노드로의 최단거리
-to_x = [float('inf')] * (N + 1)  # 각 i번 노드에서 x로의 최단거리
+"""
+X 시작 기준
+기본 그래프 다익스트라: X -> 각 노드 최단거리
+역방향 그래프 다익스트라: 각 노드 -> X 최단거리
+"""
 
-pq = []
-heappush(pq, (0, X))
+def dijkstra(start, graph) -> list[int]:
 
-# x에서 각 노드로의 최단거리 구하기
-while pq:
-    cur_dist, cur_node = heappop(pq)
-
-    if to_node[cur_node] < cur_dist:
-        continue
-
-    for dist, next_node in adj_list[cur_node]:
-        distance = cur_dist + dist
-
-        if to_node[next_node] > distance:
-            to_node[next_node] = distance
-            heappush(pq, (distance, next_node))
-
-# 각 노드에서 x로의 최단거리 구하기
-for start in range(1, N + 1):
-
-    if start == X:
-        continue
-
-    temp_distance = [float('inf')] * (N + 1)
+    distances = [float('inf')] * (N + 1)
+    pq = []
     heappush(pq, (0, start))
+
     while pq:
         cur_dist, cur_node = heappop(pq)
 
-        if temp_distance[cur_node] < cur_dist:
+        if cur_dist > distances[cur_node]:
             continue
 
-        if cur_node == X:
-            to_x[start] = cur_dist
-
-        for dist, next_node in adj_list[cur_node]:
+        for dist, next_node in graph[cur_node]:
             distance = cur_dist + dist
 
-            if temp_distance[next_node] > distance:
-                temp_distance[next_node] = distance
+            if distance < distances[next_node]:
+                distances[next_node] = distance
                 heappush(pq, (distance, next_node))
+    
+    return distances
 
-result = -float('inf')
-for i in range(1, N + 1):
-
-    if i == X:
-        continue
-
-    total_distance = to_node[i] + to_x[i]
-    result = max(result, total_distance)
-
-print(result)
+to_x = dijkstra(X, adj_list)
+to_node = dijkstra(X, rvs_list)
+total_distances = [to_x[i] + to_node[i] for i in range(1, N + 1) if i != X]
+print(max(total_distances))
