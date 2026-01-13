030c56142aa1dcdc293161fd268d7940910b70ed
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-13 15:49:27 +0900

    [Gold II] Title: 동전 분배, Time: 1516 ms, Memory: 111944 KB -BaekjoonHub

--- 백준/Gold/1943. 동전 분배/동전 분배.py
+++ 백준/Gold/1943. 동전 분배/동전 분배.py
@@ -1,6 +1,6 @@
 """
 골드 2
-DP
+DP, 배낭 문제
 """
 import sys
 input = sys.stdin.readline
@@ -13,25 +13,25 @@ for _ in range(3):
     N = int(input())
     for _ in range(N):
         coin, count = map(int, input().split())
-        coins.append([coin, count])
-        total += (coin * count)
-
+        coins.append((coin, count))
+        total += coin * count
+    
     if total % 2 == 1:
         print(0)
         continue
 
     target = total // 2
 
-    dp = [False] * (target + 1)
+    dp = [False] * (target + 2)
     dp[0] = True
 
     for coin, count in coins:
         for i in range(target, -1, -1):
             if dp[i]:
-                for k in range(1, count + 1):
-                    if i + (coin * k) <= target:
-                        dp[i + (coin * k)] = True
+                for use in range(1, count + 1):
+                    if i + (coin * use) <= target:
+                        dp[i + (coin * use)] = True
                     else:
                         break
-    
+        
     print(1) if dp[target] else print(0)
