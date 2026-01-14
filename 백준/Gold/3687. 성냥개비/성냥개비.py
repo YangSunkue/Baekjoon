a6f6efceb2d09891412db9074c3d22c5dfd107c3
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-14 16:40:35 +0900

    [Gold II] Title: 성냥개비, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.py
+++ 백준/Gold/3687. 성냥개비/성냥개비.py
@@ -1,6 +1,7 @@
 """
 골드 2
 
+그리디
 
 소모량: 숫자
 2개: 1
@@ -17,39 +18,35 @@
     N 짝수일경우 -> 1 꽉채우기
     N 홀수일경우 -> 7 한개 + 1꽉채우기
 """
-def get_min_digit_count(n):
-    """n개 성냥개비로 만들 수 있는 최소 자릿수"""
-    if n == 0:
-        return 0
-    return (n + 6) // 7
+import sys
+input = sys.stdin.readline
 
-cost = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
-
-def can_make(matches, digits):
-    """matches개로 digits 자리를 만들 수 있는지 확인"""
-    if digits == 0:
+def can_make(matches: int, digits_count: int) -> bool:
+    """matches 개 성냥으로 digits_count 자릿수를 만들 수 있는가"""
+    if digits_count == 0:
         return matches == 0
-    # digits 자리를 만들수 있는 최소/최대 개수
-    min_needed = digits * 2  # 전부 1로
-    max_needed = digits * 7  # 전부 8로
+    # digits_count 자릿수를 만들 수 있는 최소/최대 개수
+    min_needed = 2 * digits_count
+    max_needed = 7 * digits_count
     return min_needed <= matches <= max_needed
 
+
+cost = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 for _ in range(int(input())):
 
     N = int(input())
 
-    # 최소값 구하기
-    digit_count = get_min_digit_count(N)
+    digits = (N + 6) // 7
     min_result = ''
     remaining = N
 
-    for pos in range(digit_count):
+    for pos in range(digits):
         start = 1 if pos == 0 else 0
 
-        for digit in range(start, 10):
-            if can_make(remaining - cost[digit], digit_count - pos - 1):
-                min_result += str(digit)
-                remaining -= cost[digit]
+        for num in range(start, 10):
+            if can_make(remaining - cost[num], digits - pos - 1):
+                min_result += str(num)
+                remaining -= cost[num]
                 break
     
     if N % 2 == 1:
