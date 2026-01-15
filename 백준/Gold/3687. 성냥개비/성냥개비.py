ad6065de8f331193931c62b399ee11f52c69e695
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-15 13:37:03 +0900

    [Gold II] Title: 성냥개비, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/3687. 성냥개비/성냥개비.py
+++ 백준/Gold/3687. 성냥개비/성냥개비.py
@@ -1,52 +1,37 @@
 """
 골드 2
-
 그리디
-
-소모량: 숫자
-2개: 1
-3개: 7
-4개: 4
-5개: 2, 3, 5
-6개: 0, 6, 9
-7개: 8
-
-가장 작은 수:
-    그리디
-
-가장 큰 수:
-    N 짝수일경우 -> 1 꽉채우기
-    N 홀수일경우 -> 7 한개 + 1꽉채우기
 """
+
 import sys
 input = sys.stdin.readline
 
 def can_make(matches: int, digits_count: int) -> bool:
-    """matches 개 성냥으로 digits_count 자릿수를 만들 수 있는가"""
     if digits_count == 0:
         return matches == 0
-    # digits_count 자릿수를 만들 수 있는 최소/최대 개수
-    min_needed = 2 * digits_count
-    max_needed = 7 * digits_count
-    return min_needed <= matches <= max_needed
-
+    min_digits = 2 * digits_count
+    max_digits = 7 * digits_count
+    return min_digits <= matches <= max_digits
 
 cost = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
-for _ in range(int(input())):
+
+T = int(input())
+for _ in range(T):
 
     N = int(input())
 
-    digits = (N + 6) // 7
+    digits_goal = (N + 6) // 7
     min_result = ''
     remaining = N
 
-    for pos in range(digits):
-        start = 1 if pos == 0 else 0
+
+    for d in range(digits_goal):
+        start = 1 if d == 0 else 0
 
         for num in range(start, 10):
-            if can_make(remaining - cost[num], digits - pos - 1):
-                min_result += str(num)
+            if can_make(remaining - cost[num], digits_goal - d - 1):
                 remaining -= cost[num]
+                min_result += str(num)
                 break
     
     if N % 2 == 1:
