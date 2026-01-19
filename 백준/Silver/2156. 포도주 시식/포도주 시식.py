749b097642fda4c6134c28b3ede3c7f06f2e28eb
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-19 21:24:46 +0900

    [Silver I] Title: 포도주 시식, Time: 48 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.py
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.py
@@ -1,19 +1,14 @@
 """
 실버 1
 DP
-
-2잔까지만 연속으로 마실 수 있다
 """
+import sys
+input = sys.stdin.readline
+
 N = int(input())
 wine = [0, 0] + [int(input()) for _ in range(N)]
 
-"""
-dp[i][0]: 이번 포도주 안마신거
-dp[i][1]: 이번꺼 마시고 이전 포도주 안마신거
-dp[i][2]: 이번꺼 마시고 이전 포도주 마신거
-"""
 dp = [[0] * 3 for _ in range(N + 2)]
-
 for i in range(2, N + 2):
     dp[i][0] = max(dp[i - 1])
     dp[i][1] = dp[i - 1][0] + wine[i]
