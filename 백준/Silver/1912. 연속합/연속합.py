95f76459f11ec4d627b5fb4148e3d395f074ffed
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-20 17:08:55 +0900

    [Silver II] Title: 연속합, Time: 56 ms, Memory: 39096 KB -BaekjoonHub

--- 백준/Silver/1912. 연속합/연속합.py
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -8,9 +8,8 @@ input = sys.stdin.readline
 N = int(input())
 nums = list(map(int, input().split()))
 
-dp = [0] * N  # dp[i]: 0 ~ i까지의 i를 포함하는 최대 연속합
+dp = [0] * N
 dp[0] = nums[0]
-
 for i in range(1, N):
     dp[i] = max(dp[i - 1] + nums[i], nums[i])
 
