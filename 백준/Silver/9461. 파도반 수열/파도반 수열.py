bbb4306302bb89a976c0657f8bc49456fb533b5e
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-20 19:00:31 +0900

    [Silver III] Title: 파도반 수열, Time: 28 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/9461. 파도반 수열/파도반 수열.py
@@ -0,0 +1,15 @@
+"""
+실버 3
+DP
+"""
+import sys
+input = sys.stdin.readline
+
+dp = [0] * 100
+dp[0], dp[1], dp[2] = 1, 1, 1
+for i in range(3, 100):
+    dp[i] = dp[i - 2] + dp[i - 3]
+
+for _ in range(int(input())):
+    N = int(input())
+    print(dp[N - 1])
