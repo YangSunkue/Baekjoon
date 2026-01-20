7203ed5f0c2eaf68488ac1d0cab5a99a9f704f82
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-20 16:04:08 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 44 ms, Memory: 32544 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.py
@@ -0,0 +1,17 @@
+"""
+실버 3
+DP
+"""
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+dp = [0] * N
+dp[0] = 1
+if N > 1:
+    dp[1] = 3
+
+for i in range(2, N):
+    dp[i] = (dp[i - 1] + (dp[i - 2]) * 2) % 10007
+
+print(dp[-1] % 10007)
