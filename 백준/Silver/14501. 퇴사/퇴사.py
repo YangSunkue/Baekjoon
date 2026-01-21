b280349da8819857a699b77ad5d4a978b0c5d01f
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-21 13:36:02 +0900

    [Silver III] Title: 퇴사, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.py
+++ 백준/Silver/14501. 퇴사/퇴사.py
@@ -8,20 +8,16 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-T = [0] * (N + 1)  # 소요시간
-P = [0] * (N + 1)  # 상담비용
+T = [0] * (N + 1)
+P = [0] * (N + 1)
 
 for i in range(1, N + 1):
     t, p = map(int, input().split())
     T[i] = t
     P[i] = p
 
-# dp[i]: i일부터 끝까지 얻을 수 있는 최대비용
-dp = [0] * (N + 2)  # dp[N + 1] = 0 (퇴사일)
-
+dp = [0] * (N + 2)
 for i in range(N, 0, -1):
-
-    # i일 상담이 퇴사전에 끝나는 경우 상담 진행
     if i + T[i] <= N + 1:
         dp[i] = max(P[i] + dp[i + T[i]], dp[i + 1])
     else:
