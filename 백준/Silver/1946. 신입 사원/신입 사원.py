3254ceb25d925d57b3ebd282df06a3943bb5de02
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-21 14:39:04 +0900

    [Silver I] Title: 신입 사원, Time: 3524 ms, Memory: 73272 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -5,14 +5,15 @@
 import sys
 input = sys.stdin.readline
 
-for _ in range(int(input())):
+T = int(input())
+for _ in range(T):
     N = int(input())
     data = [list(map(int, input().split())) for _ in range(N)]
+    data.sort()
 
-    data.sort()  # 서류 기준으로 정렬
     min_interview = data[0][1]
+
     result = 0
-    
     for _, interview in data:
         if interview <= min_interview:
             result += 1
