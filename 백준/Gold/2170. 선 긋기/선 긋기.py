688d1e8dc31c1f5d08a4708f985513cb19524f82
양선규 <160471618+YangSunkue@users.noreply.github.com>
2026-01-22 13:44:16 +0900

    [Gold V] Title: 선 긋기, Time: 3656 ms, Memory: 237036 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -7,24 +7,23 @@ input = sys.stdin.readline
 
 N = int(input())
 lines = [list(map(int, input().split())) for _ in range(N)]
-lines.sort(key=lambda x: (x[0], x[1]))
+lines.sort()
 
 cur_start = lines[0][0]
 cur_end = lines[0][1]
-total_length = 0
+total = 0
 
 for i in range(1, N):
-    next_start, next_end = lines[i]
 
-    # 선이 겹치거나 이어지면 끝점만 확장
+    next_start = lines[i][0]
+    next_end = lines[i][1]
+
     if next_start <= cur_end:
         cur_end = max(cur_end, next_end)
-    
-    # 선이 끊어지면 기존 선 정산하고 새로 시작
     else:
-        total_length += (cur_end - cur_start)
+        total += (cur_end - cur_start)
         cur_start = next_start
         cur_end = next_end
 
-total_length += (cur_end - cur_start)
-print(total_length)
+total += (cur_end - cur_start)
+print(total)
